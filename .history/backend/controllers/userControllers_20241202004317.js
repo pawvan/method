679336f607f85b4c624f540faf69205311f84081
xiter22222 <pawanpediredla@gmail.com>
2024-12-02 00:44:17 +0530

    user controller

---
+++ .history/backend/controllers/userControllers_20241202004317.js
@@ -0,0 +1,43 @@
+/*!
+ * This code is belongs to Pawvan
+ * 
+ * 
+ * Rules:
+ * 1. You can use, modify, and distribute this code freely for non-commercial purposes.
+ * 2. Attribution must be provided in any derived works.
+ * 3. You may not use this code in any project that violates any laws.
+ * 4. No warranty is provided. Use this code at your own risk.
+ * 5. If you make any changes, you must document them.
+ * 6. Commercial use requires explicit permission from the author.
+ * 7. Redistribution of the code must include authors notice.
+ * 8. You cannot sublicense or sell this code.
+ * 9. You may not use this code in any harmful or malicious way.
+ *10. For more details, please contact: [pawanpediredla@gmail.com]
+ */
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jwt')
+exports.registerUser  = async (req,res)=>{
+    const {username,email,password} = req.body;
+    try{
+        const user  = new User({
+            username,email,password
+        })
+        const salt = await bcrypt.genSalt(10);
+        user.password=await bcrypt.hash(password,salt);
+ //save to database 
+ await user.save();
+ const token=jwt.sign({
+    id:user._id
+ },process.env.JWT_SECRET,{expiresIn:'1h'})
+
+ res,status(201).json({
+    token
+ })
+}
+    catch(error){
+    console.error(error);
+    res.status(500)
+    
+    }
+}
